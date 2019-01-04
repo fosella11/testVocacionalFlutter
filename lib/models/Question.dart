8de14c453b1db07336becbd7fbba3305c500bd9d
Federico Osella <fosella11@gmail.com>
2019-01-05 10:49:39 +1300

    migrating existing app. from native code to flutter, first commit only model question

---
+++ lib/models/Question.dart
@@ -0,0 +1,57 @@
+class Question {
+
+  int _id;
+  String _tittle;
+  bool _response;
+  bool _responded;
+  int _amount;
+
+  Question(this._tittle, this._responded, this._response, this._amount);
+
+  Question.withId(this._id, this._tittle, this._responded, this._response, this._amount);
+
+  int get id => _id;
+
+  String get title => _tittle;
+
+  bool get response => _response;
+
+  bool get responded => _responded;
+
+  int get amount => _amount;
+
+  set response(bool updateResponse){
+    this._response = updateResponse;
+  }
+
+  set responded(bool updateResponded){
+    this._responded = updateResponded;
+  }
+
+  //Convert the Questions into a map of questions
+  Map<String, dynamic> toMap(){
+    var map = Map<String, dynamic>();
+
+    if(id != null) {
+      map['id'] = _id;
+    }
+
+    map['title'] = _tittle;
+    map['response'] = _response;
+    map['responsed'] = _responded;
+    map['amount'] = _amount;
+
+    return map;
+  }
+
+  //Just in Case, if we need extract Question from map
+
+  Question.fromMapObject(Map<String, dynamic> map){
+    this._id = map['id'];
+    this._tittle = map['title'];
+    this._response = map['response'];
+    this._responded = map['responded'];
+    this._amount = map['amount'];
+  }
+
+}
